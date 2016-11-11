cda26298cf0d1bbc95c5b9661e4eb1818a1d6894
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-11-11 12:55:04 +0200

    Worker class

---
+++ src/Worker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Created by anastasia on 11/11/2016.
+ */
+public class Worker {
+
+    private String username;
+    private int workerId;
+
+    public Worker () {
+        this.username = this.setUsername();
+        this.workerId = ;
+    }
+
+    public void setUsername (Scanner reader) {
+        this.username = reader.nextLine();
+
+    }
+
+
+
+}
